469d8aaf2be4d31b9530ba8bf07121ddb597acba
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-05-10 13:06:40 -0500

    Paso 4) Creando funcionalidad de encriptador y condicionales para mostrar texto encriptado

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+// creamos la funcón para el botón encriptar
+function encriptar(){
+
+    textoEncriptar = document.getElementById("texto").value;
+    
+}
+    innerHTML = encriptar;
+
+    function encriptar () {
+        var texto = document.querySelector("#textarea").value;
+        var textoCifrado = texto.replace (/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+        document.querySelector("#textarea-resultado").value = textoCifrado;
+        document.querySelector("#textarea").value;
+        if (textoCifrado == texto || textoCifrado) {
+            document.querySelector(".container_no_found").style.display = "none";
+            document.querySelector(".textarea-resultado").style.display = "block";
+        } else {
+            document.querySelector(".container_no_found").style.display = "block";
+            document.querySelector(".textarea-resultado").style.display = "none";
+        }
+    }
+    var boton1 = document.querySelector("#encriptar"); boton1.onclick = encriptar;
+    
+    function desencriptar(){
+        var texto = document.querySelector("#textarea").value;
+        var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u"); 
+        document.querySelector("#textarea-resultado").value = textoCifrado;
+        document.querySelector("#textarea").value;
+        if (textoCifrado == texto || textoCifrado) {
+            document.querySelector(".container_no_found").style.display = "none";
+            document.querySelector(".textarea-resultado").style.display = "block";
+        } else {
+            document.querySelector(".container_no_found").style.display = "block";
+            document.querySelector(".textarea-resultado").style.display = "none";
+        } 
+    }
+    var boton2 = document.querySelector("#desencriptar"); boton2.onclick = desencriptar;
